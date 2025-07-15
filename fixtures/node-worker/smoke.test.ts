0c7cf36cd1ea207740c48a8659b318bd93a777e5
Jacob Ebey <jacob.ebey@live.com>
2025-07-14 23:32:43 -0700

    skip failing test

--- fixtures/node-worker/smoke.test.ts
+++ fixtures/node-worker/smoke.test.ts
@@ -2,23 +2,22 @@ import { expect, test } from "@playwright/test";
 
 import { setupTest } from "../test-helpers";
 
-// TODO: Figure out how to enable dev mode for node workers
-// test("basic smoke test dev", async ({ page: _page }) => {
-//   using page = await setupTest(_page, "dev", import.meta.dirname);
+test.skip("basic smoke test dev", async ({ page: _page }) => {
+  using page = await setupTest(_page, "dev", import.meta.dirname);
 
-//   const response = await page.goto("/");
-//   expect(response?.status()).toBe(200);
-//   expect(await response?.json()).toEqual({
-//     clientOnlyValue: "client-only-value",
-//     server: {
-//       serverOnlyValue: "server-only-value",
-//       sharedValue: "shared-value",
-//       value: "server-value",
-//     },
-//     sharedValue: "shared-value",
-//     value: "client-value",
-//   });
-// });
+  const response = await page.goto("/");
+  expect(response?.status()).toBe(200);
+  expect(await response?.json()).toEqual({
+    clientOnlyValue: "client-only-value",
+    server: {
+      serverOnlyValue: "server-only-value",
+      sharedValue: "shared-value",
+      value: "server-value",
+    },
+    sharedValue: "shared-value",
+    value: "client-value",
+  });
+});
 
 test("basic smoke test prod", async ({ page: _page }) => {
   using page = await setupTest(_page, "prod", import.meta.dirname);
